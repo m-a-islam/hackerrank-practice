05a96538b830933dfd396de3ed074200a1ec9db4
muhammad <muhammad.cse36@gmail.com>
2022-07-17 22:14:42 +0200

    2-min-max-sum solved

--- 2-mini-max-sum/mini-max-sum.js
+++ 2-mini-max-sum/mini-max-sum.js
@@ -11,6 +11,25 @@ function main() {
 
 
 function minMaxSum(arr) {
-   console.log(arr);
+   const max = Math.max.apply(null, arr);
+   const min = Math.min.apply(null, arr);
 
+   let maxSum = 0;
+   let minSum = 0;
+   if (max === min){
+       for (let i = 0; i < 4; i++) {
+            minSum += parseInt(arr[i], 10);
+            maxSum += parseInt(arr[i], 10);
+       }
+   }else {
+       for (let i = 0; i < arr.length; i++) {
+           if (parseInt(arr[i], 10) !== max) {
+               minSum += parseInt(arr[i], 10);
+           }
+           if (parseInt(arr[i], 10) !== min){
+               maxSum += parseInt(arr[i], 10);
+           }
+       }
+   }
+   console.log(minSum,maxSum);
 }
